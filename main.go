13fa7538bc967db6ab23bf18be5499d864d49344
quanchobi <quanchobi@proton.me>
2026-02-08 23:00:46 -0500

    Add: health endpoint that always shows healthy (very useful)

--- main.go
+++ main.go
@@ -1,22 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 const port string = "8080"
-const root string = "/"
+const root string = "/app"
+
+// Handler function for the readiness endpoint /healthz
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle(root, http.FileServer(http.Dir(".")))
+	mux.Handle(root+"/", http.StripPrefix(root, http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz/", handlerReadiness)
+
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+
 	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 }
