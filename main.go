604d556d48854e726b5d950767e6782867aa6ed3
Jacob Hanks <quanchobi@proton.me>
2026-03-23 20:07:20 -0400

    update main.go

--- main.go
+++ main.go
@@ -1,27 +1,70 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const port string = "8080"
 const root string = "/app"
 
-// Handler function for the readiness endpoint /healthz
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := int(cfg.fileserverHits.Load())
+	_, err := w.Write([]byte(fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, hits)))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		fmt.Printf("Hit! Current hits: %d\n", int(cfg.fileserverHits.Load()))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("Metrics reset."))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	mux := http.NewServeMux()
-	mux.Handle(root+"/", http.StripPrefix(root, http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz/", handlerReadiness)
+	mux.Handle(root+"/", cfg.middlewareMetricsInc(http.StripPrefix(root, http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", cfg.handleReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
